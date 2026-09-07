7fbac15413609c9edc25bd929225860e706ddf44
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-09-07 16:42:01 +0530

    fix: remove not null from issuer column in account

--- src/lib/db/schema/auth.schema.ts
+++ src/lib/db/schema/auth.schema.ts
@@ -69,7 +69,7 @@ export const account = pgTable(
   'account',
   {
     id: text('id').primaryKey(),
-    issuer: text('issuer').notNull(),
+    issuer: text('issuer'),
     accountId: text('account_id').notNull(),
     providerId: text('provider_id').notNull(),
     userId: text('user_id')
